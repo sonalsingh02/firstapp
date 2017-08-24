466b53e5ee4956a1ef37ea63a0f64091aa04a93a
sonalsingh02 <sonal@maropost.com>
2017-08-24 12:28:36 +0200

    added forget password feature

--- app/mailers/acknowledgement_mailer.rb
+++ app/mailers/acknowledgement_mailer.rb
@@ -2,5 +2,9 @@ class AcknowledgementMailer < ApplicationMailer
 	def thankyou_email(contact)
 	    @contact = contact
 	    mail(to: @contact.email, subject: 'Thank You For Contacting Us')
-  end
+  	end
+ 	def registration_mail(user)
+ 		@user = user
+ 		mail(to: @user.email, subject: 'You have successsfully registered with us')
+  	end
 end
