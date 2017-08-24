5aedb1e57f270434887aced80c38265088dab5b0
sonalsingh02 <sonal@maropost.com>
2017-08-24 11:56:48 +0200

    controller modified for sending random password to user after signup feature

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,20 +1,18 @@
 class RegistrationsController < Devise::RegistrationsController
+
   def create
     build_resource(sign_up_params)
-    resource.save
-    if resource.persisted?
-      if resource.active_for_authentication?
-        set_flash_message! :notice, :signed_up
-        sign_up(resource_name, resource)
-        respond_with resource, location: after_sign_up_path_for(resource)
-      else
-        set_flash_message! :notice, :signed_up_but_unconfirmed
-        redirect_to new_user_session_path
-      end
+    if resource.save
+      sign_out resource
+      set_flash_message :notice, :signup_confirm
+      redirect_to new_user_session_path
     else
-      clean_up_passwords resource
-      set_minimum_password_length
-      respond_with resource
+      set_flash_message :notice, :signup_error
+      redirect_to new_user_registration_path
     end
+  end
+  private 
+  def registration_params
+    params.require(:user).permit(:email)
   end
 end
