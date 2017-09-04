be7f6a561bbe17a9b9d460c4f916645b27a91307
sonalsingh02 <sonal@maropost.com>
2017-09-04 09:59:11 +0200

    git merge conflict issue resolved

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   after_create :send_email
@@ -7,16 +7,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :trackable, :validatable
 
-  
-=======
-  has_many :galleries
-  after_create :send_email
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
->>>>>>> d60ebda7dd4355b1edf94bb707631764016bc6b6
   def password_required?
     new_record? ? false : super
   end
@@ -27,16 +17,11 @@ class User < ApplicationRecord
     self.update_attributes(:password => generate_password)
     AcknowledgementMailer.registration_mail(self).deliver_now
   end
-<<<<<<< HEAD
+
   def generate_password
     password = Faker::Base.regexify(/[a-z]{2}[0-9]{7}[A-Z]{1}/)
   end
    
 end
-=======
 
-  def generate_password
-    Faker::Base.regexify(/[a-z]{2}[0-9]{7}[A-Z]{1}/)
-  end
-end
->>>>>>> d60ebda7dd4355b1edf94bb707631764016bc6b6
+
