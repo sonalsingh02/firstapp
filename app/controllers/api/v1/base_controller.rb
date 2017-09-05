387feadcb3c28f4afdb143aed03f024f268ba2ea
sonalsingh02 <sonal@maropost.com>
2017-09-05 16:41:43 +0200

    task 3: api controller modified

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,9 +1,9 @@
-class Api::V1::BaseController < ActionController::API
-	protect_from_forgery with: :null_session
-	before_action :authenticate_user! 
-	before_action :destroy_session
-	
-	def destroy_session
-    	request.session_options[:skip] = true
-  	end
+class Api::V1::BaseController < ApplicationController
+    protect_from_forgery with: :null_session
+    skip_before_action :authenticate_user!
+    before_action :destroy_session
+
+    def destroy_session
+        request.session_options[:skip] = true
+  end
 end
