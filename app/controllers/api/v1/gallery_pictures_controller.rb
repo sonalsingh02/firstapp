f7179343727f37660d4ed405edd085191bba72f6
sonalsingh02 <sonal@maropost.com>
2017-09-05 16:42:06 +0200

    task 3: api controller modified

--- app/controllers/api/v1/gallery_pictures_controller.rb
+++ app/controllers/api/v1/gallery_pictures_controller.rb
@@ -1,27 +1,42 @@
-class API::V1::GalleryPicturesController < ApplicationController
-   before_action :find_current_user
-
+class Api::V1::GalleryPicturesController < Api::V1::BaseController
+  skip_before_action :verify_authenticity_token, only: [:upload,:retrieve]
+  before_action :find_user
+  
   def upload
-    @gallery = @user.galleries.build(gallery_params)
-    if @gallery.save
-      render :json => { :status => "Success", :message => "Successful", :code => 200 }
+    if !@user.nil?
+      gallery = @user.galleries.build(upload_params)
+      if gallery.save
+        render json: { status: "Success", message: "Successful", code: 200 }
+      else
+        render json: { status: "Failure", message: gallery.errors.full_messages, code: 500 }
+      end
     else
-      render :json => { :status => "Failure", :message => gallery.errors.full_messages, :code => 500 }
+       render json: { status: "Failure", message: gallery.errors.full_messages, code: 500 }
     end
   end
- 
+
   def retrieve
-   
+    if !@user.nil?
+      gallery = @user.galleries.find_by(id: params[:id],user_id: params[:user_id])
+      if gallery
+        data = { name: gallery.name, image: gallery.image }
+        render json: { :status => "Success", :message => 'Successful', :data => data, :code => 200}
+      else
+        render json: { :status => "Failure", :message => "Gallery doesn't exist", :code => 500}
+      end
+    else
+      render json: { status: "Failure", message: "Gallery doesn't exist", code: 500 }
+    end
   end
 
   private
 
-  def find_current_user
-    @user = current_user
+  def find_user
+    @user = User.find_by(id: params[:user_id])
   end
 
-  def gallery_params
-    params.require(:gallery).permit(
+  def upload_params
+    params.require(:gallery_picture).permit(
       :name, :image, :user_id
     )
   end
