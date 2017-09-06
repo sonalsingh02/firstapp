6a78c2c0dc8261b36123ae94defd69798e68da38
sonalsingh02 <sonal@maropost.com>
2017-09-06 16:36:05 +0200

    Task 3: validations for params

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -2,11 +2,25 @@ class Api::V1::ContactsController < Api::V1::BaseController
   skip_before_action :verify_authenticity_token, only: [:send_message]
 
   def send_message
-    contact = Contact.new(contact_params)
-    if contact.save
-      render json: { status: "Success", message: "Successful", code: 200 }
+    @errors = []
+    validate_contact_params(contact_params)
+    if @errors.empty?
+      contact = Contact.new(contact_params)
+      if contact.save
+        render json: { status: "Success", message: "Successful", code: 200 }
+      else
+        render json: { status: "Failure", message: contact.errors.full_messages, code: 500 }
+      end
     else
-      render json: { status: "Failure", message: contact.errors.full_messages, code: 500 }
+      if @errors.length == 1
+        render json: { status: "Failure", message: @errors, code: 500 }
+      else
+        message = ""
+        @errors.each do |e|
+          message += e + ";"
+        end
+        render json: { :status => "Failure", :message => message, :code => 500 }
+      end
     end
   end
 
@@ -14,5 +28,38 @@ class Api::V1::ContactsController < Api::V1::BaseController
 
   def contact_params
     params.require(:contact).permit(:name, :email, :mobile_number, :message)
+  end
+
+  def all_letters_or_digits(str)
+    str[/[a-zA-Z0-9]+/]  == str
+  end
+
+  def validate_email(str)
+    str[/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i]  == str
+  end
+
+  
+
+   def validate_contact_params params
+    if params[:name] != ""
+      if !all_letters_or_digits(params[:name])
+        @errors << "Name is invalid"
+      end
+    else
+       @errors << "Name is empty"
+    end
+    if params[:email] != ""
+      if !validate_email(params[:email])
+        @errors << "Email is invalid"
+      end
+    else
+       @errors << "Email is empty"
+    end
+    if params[:mobile_number] == ""
+      @errors << "Mobile Number is empty"
+    end
+    if params[:message] == ""
+       @errors << "Message is empty"
+    end
   end
 end
