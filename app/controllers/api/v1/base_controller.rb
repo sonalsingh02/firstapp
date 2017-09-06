6fef5eb1924c78ab92e14c5333ac82643e3b923c
sonalsingh02 <sonal@maropost.com>
2017-09-06 16:35:31 +0200

    handle routing error

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -2,8 +2,13 @@ class Api::V1::BaseController < ApplicationController
     protect_from_forgery with: :null_session
     skip_before_action :authenticate_user!
     before_action :destroy_session
-
+    
     def destroy_session
         request.session_options[:skip] = true
-  end
+  	end
+
+	rescue_from ActionController::RoutingError do |exception|
+ 		logger.error 'Routing error occurred'
+ 		render json: { :status => "Failure", :message => "Page Not Found", :code => 500 }
+	end 	
 end
