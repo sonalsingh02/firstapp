323178a799e21a0f5d379fb3cebe81dc25a872b8
sonalsingh02 <sonal@maropost.com>
2017-09-06 16:36:26 +0200

    Task 3: validations for params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,11 +2,17 @@ class Api::V1::UsersController < Api::V1::BaseController
   skip_before_action :verify_authenticity_token, only: [:create_user]
 
   def create_user
-    user = User.new(create_user_params)
-    if user.save
-      render json: { status: "Success", message: "Successful", code: 200 }
+    @errors = []
+    validate_user_params(create_user_params)
+    if @errors.empty?
+      user = User.new(create_user_params)
+      if user.save
+        render json: { status: "Success", message: "Successful", code: 200 }
+      else
+        render json: { status: "Failure", message: user.errors.full_messages, code: 500 }
+      end
     else
-      render json: { status: "Failure", message: user.errors.full_messages, code: 500 }
+       render json: { status: "Failure", message: @errors, code: 500 }
     end
   end
 
@@ -14,5 +20,19 @@ class Api::V1::UsersController < Api::V1::BaseController
 
   def create_user_params
     params.require(:user).permit(:email)
+  end
+
+  def validate_email(str)
+    str[/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i]  == str
+  end
+
+  def validate_user_params params
+    if params[:email] != ""
+      if !validate_email(params[:email])
+        @errors << "Email is invalid"
+      end
+    else
+       @errors << "Email is empty"
+    end
   end
 end
