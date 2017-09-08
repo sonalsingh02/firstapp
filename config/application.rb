797166df63d4f34a046489f038e27f0dde4472e9
sonalsingh02 <sonal@maropost.com>
2017-09-08 08:53:15 +0200

    require middleware

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,10 @@ module Demo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 	config.active_job.queue_adapter = :sidekiq
-
-
+	#config.middleware.use ActionDispatch::ParamsParser
+	#config.middleware.use 'CatchJsonParseErrors'
+	require "./app/middleware/catch_json_parse_errors.rb"
+	config.middleware.insert_before Rack::Head, CatchJsonParseErrors
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
