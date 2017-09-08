a295f471a5ca40b2ec6f5e54a1153632ed232918
sonalsingh02 <sonal@maropost.com>
2017-09-08 08:50:13 +0200

    catch route error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user! 
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
-  
+  rescue_from ActionController::RoutingError do |exception|
+ 		logger.error 'Routing error occurred'
+ 		render json: { :status => "Failure", :message => "Page Not Found", :code => 500 }
+	end 
+
   def configure_permitted_parameters
     update_attrs = [:password, :password_confirmation, :current_password]
     devise_parameter_sanitizer.permit :account_update, keys: update_attrs
