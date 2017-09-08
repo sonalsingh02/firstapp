66c5ebf33b7e8fb0a2f5a1fbffe52037f6be1009
sonalsingh02 <sonal@maropost.com>
2017-09-08 08:44:51 +0200

    refactored

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -7,8 +7,5 @@ class Api::V1::BaseController < ApplicationController
         request.session_options[:skip] = true
   	end
 
-	rescue_from ActionController::RoutingError do |exception|
- 		logger.error 'Routing error occurred'
- 		render json: { :status => "Failure", :message => "Page Not Found", :code => 500 }
-	end 	
+		
 end
