5a1aac56b8eb06964cef8337343b46c395a89804
sonalsingh02 <sonal@maropost.com>
2017-09-08 08:48:48 +0200

    validation on json params on user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,16 +3,28 @@ class Api::V1::UsersController < Api::V1::BaseController
 
   def create_user
     @errors = []
-    validate_user_params(create_user_params)
+    check_key
+    validate_user_params(create_user_params) if @errors.empty?
     if @errors.empty?
-      user = User.new(create_user_params)
+      validated_params = [] 
+      validated_params = create_user_params
+      validated_params[:email] = validated_params[:email].to_s.strip
+      user = User.new(validated_params)
       if user.save
-        render json: { status: "Success", message: "Successful", code: 200 }
+        render json: { status: "Success", message: "User created successfully", code: 200 }
       else
-        render json: { status: "Failure", message: user.errors.full_messages, code: 500 }
+        render json: { status: "Failure", message: user.errors.full_messages, code: 500 } #model validations
       end
     else
-       render json: { status: "Failure", message: @errors, code: 500 }
+      if @errors.length == 1
+        render json: { status: "Failure", message: @errors, code: 500 }
+      else
+        message = ""
+        @errors.each do |e|
+          message += e + ";"
+        end
+        render json: { :status => "Failure", :message => message, :code => 500 }
+      end
     end
   end
 
@@ -27,12 +39,18 @@ class Api::V1::UsersController < Api::V1::BaseController
   end
 
   def validate_user_params params
-    if params[:email] != ""
-      if !validate_email(params[:email])
+   unless params[:email].to_s.strip.empty?
+      if !validate_email(params[:email].to_s.strip)
         @errors << "Email is invalid"
       end
     else
        @errors << "Email is empty"
+    end
+  end
+
+  def check_key
+    if(!params.has_key?(:email))
+      @errors << "Email key is missing"
     end
   end
 end
