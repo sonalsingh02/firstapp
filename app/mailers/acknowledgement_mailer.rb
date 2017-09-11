0f6d98ad471a8ceb2815c4b2a262f410f90ba9bb
sonalsingh02 <sonal@maropost.com>
2017-09-11 16:33:43 +0200

    sidekiq implementation on contact

--- app/mailers/acknowledgement_mailer.rb
+++ app/mailers/acknowledgement_mailer.rb
@@ -3,8 +3,9 @@ class AcknowledgementMailer < ApplicationMailer
 	    @contact = contact
 	    mail(to: @contact.email, subject: 'Thank You For Contacting Us')
   	end
- 	def registration_mail(user)
+ 	def registration_mail(user,password)
  		@user = user
- 		mail(to: @user.email, subject: 'You have successsfully registered with us')
+ 		@password = password
+		mail(to: @user.email, subject: 'You have successsfully registered with us')
   	end
 end
