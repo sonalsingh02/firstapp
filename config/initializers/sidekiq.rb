1e2bef92927582b63619f8466fe59801c7641c0f
sonalsingh02 <sonal@maropost.com>
2017-09-13 16:41:34 +0200

    code refactored

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,1 +1,9 @@
-Sidekiq::Extensions.enable_delay!
+#Sidekiq::Extensions.enable_delay!
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: 'redis://localhost:6379/0' }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'redis://localhost:6379/0' }
+end
