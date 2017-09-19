24dc38d458a8a56eee423b121c615f1f56dfea09
sonalsingh02 <sonal@maropost.com>
2017-09-19 13:11:18 +0200

    code refactored

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,39 +9,30 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  #SMTP
+  config.action_mailer.delivery_method = :smtp
   # Show full error reports.
   config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
-  #if Rails.root.join('tmp/caching-dev.txt').exist?
-    #config.action_controller.perform_caching = true
-
-    #config.cache_store = :memory_store
+  if Rails.root.join('tmp/caching-dev.txt').exist?
     config.cache_store = :dalli_store
     config.action_controller.perform_caching = true
-    
-   # config.public_file_server.headers = {
-    #  'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
-   # }
-  #else
-   # config.action_controller.perform_caching = false
+    config.public_file_server.headers = {
+     'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
+    }
+  else
+    config.action_controller.perform_caching = false
+    config.cache_store = :null_store
+  end
 
-    #config.cache_store = :null_store
-  #end
-
-  # Don't care if the mailer can't send.
-  #config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
-
+  
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
-  #SMTP
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }  
-  config.action_mailer.delivery_method = :smtp
+  
   # change to true to allow email to be sent during development
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
@@ -60,5 +51,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
